143ea8b06b0e491029c404e07755e86135538626
Krinesh <pkrinesh95@gmail.com>
2023-04-05 21:46:32 +0530

    refactor: cleaned up to minimum setup

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,9 @@
 <script>
-  import Header from './Header.svelte';
   import '../app.css';
-  import './styles.css';
 </script>
 
-<div class="flex flex-col min-h-screen">
-  <Header />
-
-  <main class="flex flex-1 flex-col w-full max-w-5xl mx-auto">
+<div class="flex min-h-screen">
+  <main class="flex flex-1 justify-center items-center">
     <slot />
   </main>
-
-  <footer class="flex flex-col justify-center items-center p-3 md:px-0">
-    <p>
-      visit <a class="font-bold" href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit
-    </p>
-  </footer>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,35 +1,6 @@
-<script>
-  import Counter from './Counter.svelte';
-  import welcome from '$lib/images/svelte-welcome.webp';
-  import welcome_fallback from '$lib/images/svelte-welcome.png';
-</script>
-
 <svelte:head>
   <title>Home</title>
-  <meta name="description" content="Svelte demo app" />
+  <meta name="description" content="Svelte Basic app" />
 </svelte:head>
 
-<section class="flex flex-col justify-center items-center flex-[0.6]">
-  <h1 class="w-full">
-    <span class="welcome block relative w-full h-0">
-      <picture>
-        <source srcset={welcome} type="image/webp" />
-        <img class="absolute block w-full h-full top-0" src={welcome_fallback} alt="Welcome" />
-      </picture>
-    </span>
-
-    to your new<br />SvelteKit app
-  </h1>
-
-  <h2>
-    try editing <strong>src/routes/+page.svelte</strong>
-  </h2>
-
-  <Counter />
-</section>
-
-<style>
-  .welcome {
-    padding: 0 0 calc(100% * 495 / 2048) 0;
-  }
-</style>
+<h1 class="text-3xl font-bold text-gray-700">Welcome to sveltekit</h1>
